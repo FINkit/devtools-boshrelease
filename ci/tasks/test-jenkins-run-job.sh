aa785ce43e18dbd19aa52a76ad8ec67fc2d6d3ea
Developer Environment <dan.rees@finkit.com>
2018-04-05 16:16:37 +0000

    Fix the fix to the Jenkins test

--- ci/tasks/test-jenkins-run-job.sh
+++ ci/tasks/test-jenkins-run-job.sh
@@ -7,7 +7,7 @@ BOSH_NAME=$(bosh int <(bbl outputs) --path /director_name); export BOSH_NAME
 JENKINS_PASSWORD=$(credhub get -j -n "/$BOSH_NAME/$BOSH_DEPLOYMENT/jenkins_admin_password" | jq -r .value)
 sleep 10 # Sleeping because this curl lacks the --retry-connrefused
 bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "curl -S --retry-max-time 300 --retry-delay 1 --retry 300 http://localhost:8080/login"
-cd ../../devtools-bosh-release-ci/tasks/
+cd ../../devtools-bosh-release-ci/ci/tasks/
 bosh -d $BOSH_DEPLOYMENT scp test-jenkins-run-job.xml jenkins-master/0:/tmp/test-jenkins-run-job.xml
 bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "curl -SLo /opt/jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar"
 bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 create-job print_env < /tmp/test-jenkins-run-job.xml"
