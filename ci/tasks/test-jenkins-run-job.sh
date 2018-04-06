ff7d3e7f4a358e35fd45463b4a379da65845c053
Martin Ford <ford.j.martin@gmail.com>
2018-04-06 08:29:26 +0000

    source jenkins-master helper script in jenkins test

--- ci/tasks/test-jenkins-run-job.sh
+++ ci/tasks/test-jenkins-run-job.sh
@@ -11,5 +11,5 @@ cd ../../devtools-boshrelease-ci/ci/tasks/
 bosh -d $BOSH_DEPLOYMENT scp test-jenkins-run-job.xml jenkins-master/0:/tmp/test-jenkins-run-job.xml
 bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "sudo curl -SLo /opt/jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar"
 bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "sudo chmod +r /opt/jenkins-cli.jar"
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 create-job print_env < /tmp/test-jenkins-run-job.xml"
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 build print_env -f -v"
+bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "source /var/vcap/jobs/jenkins-master/helpers/ctl_setup.sh && java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 create-job print_env < /tmp/test-jenkins-run-job.xml"
+bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "source /var/vcap/jobs/jenkins-master/helpers/ctl_setup.sh && java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 build print_env -f -v"
