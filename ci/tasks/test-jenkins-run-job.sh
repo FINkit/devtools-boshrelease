c452b36b2201b234451a8a321dd3003dc8027078
Martin Ford <ford.j.martin@gmail.com>
2018-04-06 10:13:22 +0000

    Test via an SSH proxy - WIP

--- ci/tasks/test-jenkins-run-job.sh
+++ ci/tasks/test-jenkins-run-job.sh
@@ -3,13 +3,19 @@ set -euo pipefail
 
 cd bbl-state/bbl-state
 source <(bbl print-env)
+JUMPBOX_USER="jumpbox"; export JUMPBOX_USER
+JUMPBOX_HOST=$(bosh int <(bbl outputs) --path /jumpbox_url | cut -d: -f1); export  JUMPBOX_HOST
+# set up jumpbox ssh tunnel
+instance_info=$(bosh --json instances -i); export instance_info
+jenkins_ip=$(jq -r .Tables[0].Rows[0].ips <<< "$instance_info")
+# shellcheck disable=SC2029
+ssh -o StrictHostKeyChecking=no -fNnL "8080:$jenkins_ip:8080" -i                    "$JUMPBOX_PRIVATE_KEY" "$JUMPBOX_USER@$JUMPBOX_HOST"
+trap "pkill ssh" EXIT
 BOSH_NAME=$(bosh int <(bbl outputs) --path /director_name); export BOSH_NAME
-JENKINS_PASSWORD=$(credhub get -j -n "/$BOSH_NAME/$BOSH_DEPLOYMENT/jenkins_admin_password" | jq -r .value)
+JENKINS_PASSWORD=$(credhub get -j -n "/$BOSH_NAME/jenkins/jenkins_admin_password" | jq -r .value)
 sleep 10 # Sleeping because this curl lacks the --retry-connrefused
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "curl -S --retry-max-time 300 --retry-delay 1 --retry 300 http://localhost:8080/login"
-cd ../../devtools-boshrelease-ci/ci/tasks/
-bosh -d $BOSH_DEPLOYMENT scp test-jenkins-run-job.xml jenkins-master/0:/tmp/test-jenkins-run-job.xml
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "sudo curl -SLo /opt/jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar"
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "sudo chmod +r /opt/jenkins-cli.jar"
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "source /var/vcap/jobs/jenkins-master/helpers/ctl_setup.sh jenkins-master && java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 create-job print_env < /tmp/test-jenkins-run-job.xml"
-bosh -d $BOSH_DEPLOYMENT ssh jenkins-master/0 -c "source /var/vcap/jobs/jenkins-master/helpers/ctl_setup.sh jenkins-master && java -jar /opt/jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 build print_env -f -v"
+curl -S --retry-max-time 300 --retry-delay 1 --retry 300 http://localhost:8080/login
+curl -SLo jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar
+chmod +r jenkins-cli.jar
+java -jar jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 create-job print_env < devtools-boshrelease-ci/ci/tasks/test-jenkins-run-job.xml
+java -jar jenkins-cli.jar -auth administrator:$JENKINS_PASSWORD -s http://localhost:8080 build print_env -f -v
