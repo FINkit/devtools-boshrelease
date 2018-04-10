d646e316ec34ca9b901b0b5ae4f368a20d43b5d0
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-04-10 19:25:06 +0000

    Refactored all tests into golang

---
+++ ci/tests/sonarqubetest/godogs_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+	"os"
+	"net/http"
+	"io/ioutil"
+	"github.com/DATA-DOG/godog"
+)
+
+var url string
+var body string
+
+func thereIsASonarqubeInstall() error {
+	url = os.Getenv("SONARQUBE_HOST")
+	return nil
+}
+
+func iAccessTheLoginScreen() error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body_bytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return err
+	}
+
+	body = string(body_bytes)
+	return nil
+}
+
+func sonarQubeShouldBeUnlocked() error {
+	if !strings.Contains(body, "Log In to SonarQube") {
+		return fmt.Errorf("expected %s to contain 'Log In to SonarQube'", body)
+	}
+	return nil
+}
+
+func FeatureContext(s *godog.Suite) {
+	s.Step(`^there is a sonarqube install$`, thereIsASonarQubeInstall)
+	s.Step(`^I access the login screen$`, iAccessTheLoginScreen)
+	s.Step(`^sonarqube should be unlocked$`, sonarqubeShouldBeUnlocked)
+}
+
