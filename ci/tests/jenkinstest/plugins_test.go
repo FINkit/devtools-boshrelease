a5eb532e43812e77234038bfa0a4e2197341dd94
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-04-16 16:24:54 +0100

    Extended plugin test to test for all expected plugins

--- ci/tests/jenkinstest/plugins_test.go
+++ ci/tests/jenkinstest/plugins_test.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
 const (
-	PLUGIN_MANAGER_URL string = "/pluginManager/api/xml?depth=1"
+	PLUGIN_MANAGER_URL string = "/pluginManager/api/json?depth=1&tree=plugins[shortName,version]"
 )
+
+type Plugins struct {
+	Plugins []Plugin
+}
+
+type Plugin struct {
+	Name    string `json:"shortName"`
+	Version string `json:"version"`
+}
 
 func iAccessPluginManagement() error {
 	pluginsResp, err := httpClient.Get(getUrl(PLUGIN_MANAGER_URL))
@@ -20,9 +33,69 @@ func iAccessPluginManagement() error {
 	return nil
 }
 
+func createPluginArray(plugins Plugins) map[string]string {
+	a := make(map[string]string)
+
+	for _, plugin := range plugins.Plugins {
+		a[plugin.Name] = plugin.Version
+	}
+	return a
+}
+
+func getAllInstalledPlugins() (Plugins, error) {
+	var plugins Plugins
+	json.Unmarshal([]byte(body), &plugins)
+
+	if len(plugins.Plugins) == 0 {
+		return plugins, fmt.Errorf("%s", "No plugins installed")
+	}
+
+	return plugins, nil
+}
+
+func getAllExpectedPlugins() (Plugins, error) {
+	var plugins Plugins
+	filePath, _ := os.Getwd()
+
+	p, err := ioutil.ReadFile(filePath + "/../../../src/jenkins/plugins.txt")
+
+	if err != nil {
+		return Plugins{}, fmt.Errorf("%s", err)
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(fmt.Sprintf("%s", p)))
+	for scanner.Scan() {
+		pluginLine := scanner.Text()
+		splitPlugin := strings.Split(pluginLine, ":")
+		plugin := Plugin{Name: splitPlugin[0], Version: splitPlugin[1]}
+		plugins.Plugins = append(plugins.Plugins, plugin)
+	}
+
+	if len(plugins.Plugins) == 0 {
+		return plugins, fmt.Errorf("%s", "No expected plugins")
+	}
+
+	return plugins, nil
+}
+
 func allThePluginsAreInstalled() error {
-	if !strings.Contains(body, "<shortName>cucumber-reports</shortName>") {
-		return fmt.Errorf("expected %s to contain 'cucumber-reports'", body)
+	installedPlugins, err := getAllInstalledPlugins()
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+	installedPluginsArray := createPluginArray(installedPlugins)
+	expectedPlugins, err := getAllExpectedPlugins()
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+
+	for _, v := range expectedPlugins.Plugins {
+		if installedPluginsArray[v.Name] == "" {
+			return fmt.Errorf("Expected plugin %s is not installed", v.Name)
+		}
+		if installedPluginsArray[v.Name] != v.Version {
+			return fmt.Errorf("Unexpected plugin version %s for %s (should be version %s)", installedPluginsArray[v.Name], v.Name, v.Version)
+		}
 	}
 	return nil
 }
