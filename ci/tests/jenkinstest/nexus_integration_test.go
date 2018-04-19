1794d9b799aee3798e34eae2c2eb5df9874a627a
Bryan Wiggins <bryan.oakley-wiggins@finkit.com>
2018-04-19 16:15:03 +0100

    added maven nexus integration test 2

--- ci/tests/jenkinstest/nexus_integration_test.go
+++ ci/tests/jenkinstest/nexus_integration_test.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 )
 
 const (
 	MAVEN_JOB_FILE_PATH string = "./test-maven-job.xml"
 	MAVEN_JOB_NAME      string = "mavenJob"
+	NEXUS_ARTEFACT_URL  string = "https://nexus.dev-build-create.build.finkit.io/repository/releases-libs/com/example/demo/0.0.1/demo-0.0.1.jar"
 )
 
 func iHaveAddedAMavenJob() error {
@@ -41,22 +41,24 @@ func iExecuteTheMavenJob() error {
 }
 
 func theJobArtefactIsStoredInNexus() error {
+	nexusResp, err := httpClient.Get(NEXUS_ARTEFACT_URL)
+
+	fmt.Printf("nexus response from %s to %s .", NEXUS_ARTEFACT_URL, err)
+
+	if nexusResp.StatusCode == http.StatusOK {
+		return fmt.Errorf("returned 200 unexpectedly")
+	}
+
 	time.Sleep(40 * time.Second)
 
-	jobResp, err := httpClient.Get(getUrl("/job/" + MAVEN_JOB_NAME + "/1/api/json"))
+	nexusResp, err = httpClient.Get(NEXUS_ARTEFACT_URL)
 
 	if err != nil {
-		return fmt.Errorf("Failed to create job %s.", MAVEN_JOB_NAME)
+		return fmt.Errorf("Failed to retrieve artefact from %s.", NEXUS_ARTEFACT_URL)
 	}
 
-	if jobResp.StatusCode != http.StatusOK {
-		return fmt.Errorf("theJobArtefactIsStoredInNexus status code returned %d not 200", jobResp.StatusCode)
-	}
-
-	body, _ := getBodyString(jobResp)
-
-	if !strings.Contains(body, `"result":"SUCCESS"`) {
-		return fmt.Errorf("theJobArtefactIsStoredInNexus failed: %s", body)
+	if nexusResp.StatusCode != http.StatusOK {
+		return fmt.Errorf("theJobArtefactIsStoredInNexus status code returned %d not 200", nexusResp.StatusCode)
 	}
 
 	return nil
